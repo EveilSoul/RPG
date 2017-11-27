a021224f1ce92c2161a39615073f6a3527bca298
Вакатов Сергей <mr.blackpirate@yandex.ru>
2017-11-27 23:52:42 +0300

    [add] Spell.cs

--- TheGame/Spell.cs
+++ TheGame/Spell.cs
@@ -20,5 +20,28 @@
             Defence,
             Attacking
         };
+
+        public Spell(string name, int mana, int level, string description, 
+            TypeOfSpell type, params float[] param)
+        {
+            this.Name = name;
+            this.Mana = mana;
+            this.Level = level;
+            this.Description = description;
+            this.Type = type;
+
+            switch (type)
+            {
+                case TypeOfSpell.Attacking:
+                    this.CountImpact = (int)param[0];
+                    this.Damage = (int)param[1];
+                    this.Accuracy = param[2];
+                    break;
+                case TypeOfSpell.Defence:
+                    this.CountOfProtect = (int)param[0];
+                    this.Protect = param[1];
+                    break;
+            }
+        }
     }
 }
