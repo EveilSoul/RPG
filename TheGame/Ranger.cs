76e93cfd94cd1987d840c6e182ebc44104e091eb
Вакатов Сергей <mr.blackpirate@yandex.ru>
2017-11-28 19:23:07 +0300

    [add] constructor in Ranger

--- TheGame/Ranger.cs
+++ TheGame/Ranger.cs
@@ -2,5 +2,21 @@
 {
     public class Ranger : Player
     {
+        public Ranger(string name, ObjectStructures.Position position)
+        {
+            this.TravelSkill = 10;
+            this.BattleSkill = 0;
+            this.Health = 220;
+            this.Mana = 160;
+            this.Money = 200;
+            this.PowerAttack = 10;
+            this.Inventory = new object[10];
+            this.Level = 1;
+            this.Type = PlayerType.Warrior;
+            this.Accuracy = 0.8f;
+
+            this.Name = name;
+            this.Position = position;
+        }
     }
 }
