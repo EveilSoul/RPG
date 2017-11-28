2fc464cea8929ab54186023d4b5bc7fbaa689e81
Red Fox <ckoteyca@yandex.ru>
2017-11-28 19:39:06 +0300

    [change] Wizard

--- TheGame/Wizard.cs
+++ TheGame/Wizard.cs
@@ -3,10 +3,23 @@ namespace TheGame
 {
     public class Wizard : Player
     {
-        
 
-        public Wizard()
+
+        public Wizard(string name, ObjectStructures.Position position)
         {
+            this.TravelSkill = 0;
+            this.BattleSkill = 0;
+            this.Health = 200;
+            this.Mana = 200;
+            this.Money = 200;
+            this.PowerAttack = 8;
+            this.Inventory = new object[10];
+            this.Level = 1;
+            this.Type = PlayerType.Wizard;
+            this.Accuracy = 0.8f;
+
+            this.Name = name;
+            this.Position = position;
 
         }
     }
