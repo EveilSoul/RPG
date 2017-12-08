91d50e0913998f4f3c19d26004d8a9fa592c7d1c
Red Fox <ckoteyca@yandex.ru>
2017-12-08 17:54:10 +0300

    change enemy create

--- TheGame/EnemyMix.cs
+++ TheGame/EnemyMix.cs
@@ -9,13 +9,13 @@ namespace TheGame
             var enemy = new List<Enemy>();
             int count = Program.Random.Next(PlayerLevel, 4 + PlayerLevel);
 
-            for (int i = 0; i < count; i++)
+            for (int i = 0; i < (count > 16 ? 16 : count); i++)
             {
                 int rand = Program.Random.Next(1, 151 + PlayerLevel * PlayerLevel);
 
-                if (rand >= 1 && rand <= 50) enemy.Add(new EnemyWolf(PlayerLevel));
-                else if (rand > 50 && rand <= 100) enemy.Add(new EnemyGoblin(PlayerLevel));
-                else if (rand > 100 && rand <= 150) enemy.Add(new EnemyBear(PlayerLevel));
+                if (rand >= 1 && rand <= 50) enemy.Add(new EnemyWolf(count <= 16 ? PlayerLevel : PlayerLevel + count - 16));
+                else if (rand > 50 && rand <= 100) enemy.Add(new EnemyGoblin(count <= 16 ? PlayerLevel : PlayerLevel + count - 16));
+                else if (rand > 100 && rand <= 150) enemy.Add(new EnemyBear(count <= 16 ? PlayerLevel : PlayerLevel + count - 16));
                 else enemy.Add(new EnemyDragon(PlayerLevel));
             }
             return enemy;
